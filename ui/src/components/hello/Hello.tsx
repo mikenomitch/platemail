8a6b2237849768b0fccd4344d4c3fec11fd8b3cf
Michael Nomitch <mike@Michaels-MacBook-Pro.local>
2019-08-04 15:55:30 -0500

    added themes

--- ui/src/components/hello/Hello.tsx
+++ ui/src/components/hello/Hello.tsx
@@ -11,6 +11,19 @@ function getExclamationMarks(numChars: number) {
   return Array(numChars + 1).join("!");
 }
 
+function toggleTheme() {
+  const doc = document.documentElement;
+
+  const theme = doc.getAttribute("data-theme");
+  const newTheme = theme === "dark" ? "light" : "dark";
+
+  doc.classList.add("theme-transition");
+  doc.setAttribute("data-theme", newTheme);
+  window.setTimeout(() => {
+    doc.classList.remove("theme-transition");
+  }, 1000);
+}
+
 export interface IProps {
   name: string;
   enthusiasmLevel?: number;
@@ -34,8 +47,8 @@ function Hello({
         Hello {name + getExclamationMarks(enthusiasmLevel)}
       </h2>
       <p>
-        This page demonstrates actions (click the buttons), image embeds, and
-        components.
+        This page demonstrates actions (click the buttons), image embeds,
+        components, and themes.
       </p>
       <img src={danton} />
       <br />
@@ -47,14 +60,11 @@ function Hello({
       </div>
       <br />
       <br />
-
       <div>
         <TextInput label="Email" onChange={console.log.bind(console)} />
       </div>
-
       <br />
       <br />
-
       <div>
         <TextArea
           error="There is an issue"
@@ -62,6 +72,8 @@ function Hello({
           onChange={console.log.bind(console)}
         />
       </div>
+      &nbsp;&nbsp;&nbsp;
+      <Button onClick={toggleTheme}>Toggle Theme</Button>
     </div>
   );
 }
